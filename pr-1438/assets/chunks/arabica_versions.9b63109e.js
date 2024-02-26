a4e39355e860a71bbac3aff68f69568a405759cc
sysrex <sysrex@users.noreply.github.com>
2024-02-26 14:11:43 +0000

    Deploy preview for PR 1438 🛫

---
+++ pr-1438/assets/chunks/arabica_versions.9b63109e.js
@@ -0,0 +1,1 @@
+const a=Object.freeze({"app-latest-tag":"v1.6.0","app-latest-sha":"6f19fd83c4d0061c717a40e23a368a2f60aff715","core-latest-tag":"v1.33.0-tm-v0.34.29","core-latest-sha":"2fedb46770069ea0de77016c8f954b09c2f779f9","node-latest-tag":"v0.13.0","node-latest-sha":"e55e1c88708b46839867bcbbed9bcdd8a3ffa830"});export{a};
