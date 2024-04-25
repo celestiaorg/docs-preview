dc3358621d933b6c224a4b22cd48c7473054f3f2
rach-id <rach-id@users.noreply.github.com>
2024-04-25 15:50:21 +0000

    Deploy preview for PR 1543 🛫

---
+++ pr-1543/assets/nodes_validator-node.md.db5ef4f6.lean.js
@@ -0,0 +1,37 @@
+import{c as o}from"./chunks/constants.dc70dfe7.js";import{o as t,c as p,k as s,a,t as e,l,Q as n}from"./chunks/framework.71081fb7.js";const c="/docs-preview/pr-1543/img/nodes/validator.png",i=n("",19),r={class:"language-bash vp-adaptive-theme"},d=s("button",{title:"Copy Code",class:"copy"},null,-1),_=s("span",{class:"lang"},"bash",-1),h={class:"shiki github-dark vp-code-dark"},y=n("",1),u=n("",1),E={class:"line"},C=s("span",{style:{color:"#E1E4E8"}},"--from=$VALIDATOR_WALLET ",-1),g={style:{color:"#79B8FF"}},F=s("span",{style:{color:"#E1E4E8"}}," ",-1),T=s("span",{style:{color:"#79B8FF"}},"\\",-1),b=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"--fees=21000utia")],-1),m={class:"shiki github-light vp-code-light"},v=n("",1),A=n("",1),f={class:"line"},k=s("span",{style:{color:"#24292E"}},"--from=$VALIDATOR_WALLET ",-1),S={style:{color:"#005CC5"}},q=s("span",{style:{color:"#24292E"}}," ",-1),B=s("span",{style:{color:"#005CC5"}},"\\",-1),I=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"--fees=21000utia")],-1),w=n("",28),P={class:"language-bash vp-adaptive-theme"},V=s("button",{title:"Copy Code",class:"copy"},null,-1),x=s("span",{class:"lang"},"bash",-1),D={class:"shiki github-dark vp-code-dark"},R=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"MONIKER"),s("span",{style:{color:"#F97583"}},"="),s("span",{style:{color:"#9ECBFF"}},'"your_moniker"')],-1),N=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"VALIDATOR_WALLET"),s("span",{style:{color:"#F97583"}},"="),s("span",{style:{color:"#9ECBFF"}},'"validator"')],-1),L=s("span",{class:"line"},null,-1),O=n("",1),W=n("",1),$=n("",1),z=n("",1),M={class:"line"},Y=s("span",{style:{color:"#E1E4E8"}},"    ",-1),U={style:{color:"#79B8FF"}},K=s("span",{style:{color:"#E1E4E8"}}," ",-1),G=s("span",{style:{color:"#79B8FF"}},"\\",-1),H=n("",1),j=n("",1),J=n("",1),Q=n("",1),X=n("",1),Z=n("",1),ss=n("",1),as=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"    "),s("span",{style:{color:"#79B8FF"}},"--gas=220000")],-1),ns={class:"shiki github-light vp-code-light"},os=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"MONIKER"),s("span",{style:{color:"#D73A49"}},"="),s("span",{style:{color:"#032F62"}},'"your_moniker"')],-1),es=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"VALIDATOR_WALLET"),s("span",{style:{color:"#D73A49"}},"="),s("span",{style:{color:"#032F62"}},'"validator"')],-1),ls=s("span",{class:"line"},null,-1),ts=n("",1),ps=n("",1),cs=n("",1),is=n("",1),rs={class:"line"},ds=s("span",{style:{color:"#24292E"}},"    ",-1),_s={style:{color:"#005CC5"}},hs=s("span",{style:{color:"#24292E"}}," ",-1),ys=s("span",{style:{color:"#005CC5"}},"\\",-1),us=n("",1),Es=n("",1),Cs=n("",1),gs=n("",1),Fs=n("",1),Ts=n("",1),bs=n("",1),ms=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"    "),s("span",{style:{color:"#005CC5"}},"--gas=220000")],-1),vs=n("",11),Is=JSON.parse('{"title":"Setting up a Celestia validator node","description":"Learn how to set up a Celestia validator node.","frontmatter":{"description":"Learn how to set up a Celestia validator node.","outline":"deep","head":[["meta",{"name":"og:title","content":"Setting up a Celestia validator node | Celestia Docs"},{"name":"og:description","content":false}]]},"headers":[],"relativePath":"nodes/validator-node.md","filePath":"nodes/validator-node.md","lastUpdated":1714060166000}'),As={name:"nodes/validator-node.md"},ws=Object.assign(As,{setup(fs){return(ks,Ss)=>(t(),p("div",null,[i,s("div",r,[d,_,s("pre",h,[s("code",null,[y,a(`
+`),u,a(`
+`),s("span",E,[C,s("span",g,"--chain-id="+e(l(o).mochaChainId),1),F,T]),a(`
+`),b])]),s("pre",m,[s("code",null,[v,a(`
+`),A,a(`
+`),s("span",f,[k,s("span",S,"--chain-id="+e(l(o).mochaChainId),1),q,B]),a(`
+`),I])])]),w,s("div",P,[V,x,s("pre",D,[s("code",null,[R,a(`
+`),N,a(`
+`),L,a(`
+`),O,a(`
+`),W,a(`
+`),$,a(`
+`),z,a(`
+`),s("span",M,[Y,s("span",U,"--chain-id="+e(l(o).mochaChainId),1),K,G]),a(`
+`),H,a(`
+`),j,a(`
+`),J,a(`
+`),Q,a(`
+`),X,a(`
+`),Z,a(`
+`),ss,a(`
+`),as])]),s("pre",ns,[s("code",null,[os,a(`
+`),es,a(`
+`),ls,a(`
+`),ts,a(`
+`),ps,a(`
+`),cs,a(`
+`),is,a(`
+`),s("span",rs,[ds,s("span",_s,"--chain-id="+e(l(o).mochaChainId),1),hs,ys]),a(`
+`),us,a(`
+`),Es,a(`
+`),Cs,a(`
+`),gs,a(`
+`),Fs,a(`
+`),Ts,a(`
+`),bs,a(`
+`),ms])])]),vs]))}});export{Is as __pageData,ws as default};
