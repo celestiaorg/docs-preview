ba2ae5fe64a0533d53dfbcae05030b2023ea2c48
Wondertan <Wondertan@users.noreply.github.com>
2024-05-07 13:51:41 +0000

    Deploy preview for PR 1564 🛫

---
+++ pr-1564/assets/chunks/arabica_versions.f433e621.js
@@ -0,0 +1,1 @@
+const a=Object.freeze({"app-latest-tag":"v1.9.0","app-latest-sha":"ad18465a88000a5b45ceb63efc7d46efaaa56f2c","core-latest-tag":"v1.35.0-tm-v0.34.29","core-latest-sha":"f95a434e674610b3a2c5e7d26bd87da69ccaa213","node-latest-tag":"v0.13.4","node-latest-sha":"13ac02fe9a659b8741c2f62fac256c4ef249b718"});export{a};
