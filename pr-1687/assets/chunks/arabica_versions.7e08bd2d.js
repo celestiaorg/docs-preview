f4d23218106a6baffd477fe2ba92f99c6edb0805
jcstein <jcstein@users.noreply.github.com>
2024-08-26 16:09:11 +0000

    Deploy preview for PR 1687 🛫

--- pr-1687/assets/chunks/arabica_versions.7e08bd2d.js
+++
@@ -1,1 +0,0 @@
-const e=Object.freeze({"app-latest-tag":"v2.1.2","app-latest-sha":"48173df3dc78f9348eedb3796f29ef9e9e5dc584","core-latest-tag":"v1.40.1-tm-v0.34.29-rc0","core-latest-sha":"0d2b63836d0f4587e162bfded58f53fba238e69c","node-latest-tag":"v0.16.1-rc0","node-latest-sha":"9fd66a56132d25957fd25116ca548343fee0f3c4"});export{e as a};
