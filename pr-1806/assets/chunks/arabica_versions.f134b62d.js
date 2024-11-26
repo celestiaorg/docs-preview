539ad355c58fb09e4efe0f2741d850ce29a6e39d
jcstein <jcstein@users.noreply.github.com>
2024-11-26 13:43:06 +0000

    Deploy preview for PR 1806 🛫

---
+++ pr-1806/assets/chunks/arabica_versions.f134b62d.js
@@ -0,0 +1,1 @@
+const a=Object.freeze({"app-latest-tag":"v3.0.1-arabica","app-latest-sha":"dbd27087fadd80171e65c30aef776bc5895cb8da","core-latest-tag":"v1.43.0-tm-v0.34.35","core-latest-sha":"c2c6463a79697d511650d5113738acf5861ba7d7","node-latest-tag":"v0.20.2-arabica","node-latest-sha":"2dc9cce7b5dffc14fd4fe156d2aa9869318e0f68"});export{a};
