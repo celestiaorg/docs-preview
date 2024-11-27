3ff5e82aac4ce88b4e01ac31c95c8ef9ddc1fbe3
jcstein <jcstein@users.noreply.github.com>
2024-11-27 14:18:53 +0000

    Deploy preview for PR 1807 🛫

---
+++ pr-1807/assets/chunks/mainnet_versions.ccccab9b.js
@@ -0,0 +1,1 @@
+const e=Object.freeze({"app-latest-tag":"v2.3.1","app-latest-sha":"06f24f5dbe48c29964e1d8cfb030cffd90797ded","core-latest-tag":"v1.41.0-tm-v0.34.29","core-latest-sha":"aef322775c75783488b439cdf7ca33f38da08426","node-latest-tag":"v0.20.3","node-latest-sha":"f5d9b3207dfc6e9a39c0dbb2fa19ef6f96eacf6f"});export{e as m};
