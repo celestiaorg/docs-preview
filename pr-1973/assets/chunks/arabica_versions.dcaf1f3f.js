a0be192ac75c924d73d777909d7943e6ff6390a0
jcstein <jcstein@users.noreply.github.com>
2025-04-09 16:23:25 +0000

    Deploy preview for PR 1973 🛫

--- pr-1973/assets/chunks/arabica_versions.dcaf1f3f.js
+++
@@ -1,1 +0,0 @@
-const a=Object.freeze({"app-latest-tag":"v3.8.1-arabica","app-latest-sha":"a91f373fba51476c72982618108c7b8a1126c757","core-latest-tag":"v1.53.0-tm-v0.38.17-rc1","core-latest-sha":"6aac2abf783f75bc7b69aaf484d9819e787ccb63","node-latest-tag":"v0.21.9-mammoth-v0.0.7","node-latest-sha":"35f0315e269ddd9ac31b0c98015f0084ab72f105"});export{a};
