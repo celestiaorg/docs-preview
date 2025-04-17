8d75d66227b15ffdd88931352f4d52d6e0362b70
jcstein <jcstein@users.noreply.github.com>
2025-04-17 14:31:19 +0000

    Remove preview for PR 1993 🛬

--- pr-1993/assets/chunks/mainnet_versions.0b003d01.js
+++
@@ -1,1 +0,0 @@
-const a=Object.freeze({"app-latest-tag":"v3.8.1","app-latest-sha":"a91f373fba51476c72982618108c7b8a1126c757","core-latest-tag":"v1.53.0-tm-v0.38.17-rc2","core-latest-sha":"092d3ad8506c67c3d138c06206cd2c41586cc1be","node-latest-tag":"v0.21.9","node-latest-sha":"3d8fb74bc073fb23303dcfc2804ab85ce316fa24"});export{a as m};
