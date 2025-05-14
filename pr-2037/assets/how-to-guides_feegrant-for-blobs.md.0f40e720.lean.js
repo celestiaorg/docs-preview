4c3f3017438855b460c3f837e6fd4d37b8f65b63
jcstein <jcstein@users.noreply.github.com>
2025-05-14 21:01:26 +0000

    Deploy preview for PR 2037 🛫

---
+++ pr-2037/assets/how-to-guides_feegrant-for-blobs.md.0f40e720.lean.js
@@ -0,0 +1,19 @@
+import{c as o}from"./chunks/constants.b02263eb.js";import{o as e,c as t,k as s,a,t as l,l as p,Q as n}from"./chunks/framework.0dcd4fb2.js";const c=n("",48),r={class:"language-bash vp-adaptive-theme"},i=s("button",{title:"Copy Code",class:"copy"},null,-1),y=s("span",{class:"lang"},"bash",-1),E={class:"shiki github-dark vp-code-dark"},F=n("",1),d=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"  $GRANTER_ADDRESS $GRANTEE_ADDRESS "),s("span",{style:{color:"#79B8FF"}},"\\")],-1),u=n("",1),h=n("",1),C=n("",1),g={class:"line"},_=s("span",{style:{color:"#E1E4E8"}},"  ",-1),b=s("span",{style:{color:"#79B8FF"}},"--chain-id",-1),B=s("span",{style:{color:"#E1E4E8"}}," ",-1),m={style:{color:"#9ECBFF"}},q=s("span",{style:{color:"#E1E4E8"}}," ",-1),f=s("span",{style:{color:"#79B8FF"}},"\\",-1),T=n("",1),A=n("",1),v=n("",1),k=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"  "),s("span",{style:{color:"#79B8FF"}},"--yes")],-1),R={class:"shiki github-light vp-code-light"},S=n("",1),D=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"  $GRANTER_ADDRESS $GRANTEE_ADDRESS "),s("span",{style:{color:"#005CC5"}},"\\")],-1),w=n("",1),P=n("",1),G=n("",1),N={class:"line"},x=s("span",{style:{color:"#24292E"}},"  ",-1),$=s("span",{style:{color:"#005CC5"}},"--chain-id",-1),I=s("span",{style:{color:"#24292E"}}," ",-1),O={style:{color:"#032F62"}},V=s("span",{style:{color:"#24292E"}}," ",-1),U=s("span",{style:{color:"#005CC5"}},"\\",-1),j=n("",1),L=n("",1),M=n("",1),H=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"  "),s("span",{style:{color:"#005CC5"}},"--yes")],-1),K=n("",27),W=JSON.parse('{"title":"FeeGrant module for blobs submission","description":"","frontmatter":{"head":[["meta",{"property":"og:title","content":"FeeGrant module for blobs submission | Celestia Docs"}],["meta",{"property":"og:description","content":"The first modular blockchain network."}]]},"headers":[],"relativePath":"how-to-guides/feegrant-for-blobs.md","filePath":"how-to-guides/feegrant-for-blobs.md","lastUpdated":1747256417000}'),Y={name:"how-to-guides/feegrant-for-blobs.md"},ss=Object.assign(Y,{setup(z){return(X,J)=>(e(),t("div",null,[c,s("div",r,[i,y,s("pre",E,[s("code",null,[F,a(`
+`),d,a(`
+`),u,a(`
+`),h,a(`
+`),C,a(`
+`),s("span",g,[_,b,B,s("span",m,l(p(o).mochaChainId),1),q,f]),a(`
+`),T,a(`
+`),A,a(`
+`),v,a(`
+`),k])]),s("pre",R,[s("code",null,[S,a(`
+`),D,a(`
+`),w,a(`
+`),P,a(`
+`),G,a(`
+`),s("span",N,[x,$,I,s("span",O,l(p(o).mochaChainId),1),V,U]),a(`
+`),j,a(`
+`),L,a(`
+`),M,a(`
+`),H])])]),K]))}});export{W as __pageData,ss as default};
