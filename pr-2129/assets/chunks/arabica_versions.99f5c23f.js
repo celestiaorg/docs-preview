885159157af7ba103e8e43d81c348a610958bbaa
rootulp <rootulp@users.noreply.github.com>
2025-07-01 20:08:38 +0000

    Deploy preview for PR 2129 🛫

---
+++ pr-2129/assets/chunks/arabica_versions.99f5c23f.js
@@ -0,0 +1,1 @@
+const a=Object.freeze({"app-latest-tag":"v4.0.6-arabica","app-latest-sha":"e8532b64d1f4fd31ea4764d8df21ee52c566abd3","node-latest-tag":"v0.23.2-arabica","node-latest-sha":"43dcbaad775cd4827c536c107d62a2c13b772bc0"});export{a};
