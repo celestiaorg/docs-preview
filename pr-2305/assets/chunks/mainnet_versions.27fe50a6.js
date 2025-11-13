ac6a94cbcd9de452d36ebd053b7886aab1cfc38d
gbarros <gbarros@users.noreply.github.com>
2025-11-13 17:15:21 +0000

    Remove preview for PR 2305 🛬

--- pr-2305/assets/chunks/mainnet_versions.27fe50a6.js
+++
@@ -1,1 +0,0 @@
-const e=Object.freeze({"app-latest-tag":"v6.2.4","app-latest-sha":"92a687234ea76eb6b4dc03c97355dc363788b057","node-latest-tag":"v0.28.2","node-latest-sha":"e4384c0f4b5de00d65e321ee9f4f400850165e6d"});export{e as m};
